c0b66dfffabc40ba8933bc5643d8b68f1bd40bc9
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-14 16:55:28 -0400

    first commit1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,5 @@
---A SQL script that creates a table users
+---- SQL script that creates a table users following these 
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
