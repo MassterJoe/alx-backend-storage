22391b3ebeceffb7f9397ad228d8d1b0a5ea740d
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-15 13:53:01 -0400

    task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
---Write a SQL script that creates a table users following these requirements:
--- id, email, name, country
-CREATE TABLE IF NOT EXISTS users(
+---- Write a SQL script that creates a table users following these requirements:
+--- id, email, name, country
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
