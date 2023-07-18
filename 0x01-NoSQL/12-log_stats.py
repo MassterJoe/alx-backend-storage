b82497b6baed908606bb6f0c3d617f6e273bbda3
Sojbaba98@gmail.com <sojbaba98@gmail.com>
2023-07-18 05:04:44 -0400

    great

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+def log_stat():
+
+    myclient = MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["logs"]
+    mycol = mydb["nginx"]
+    total_log = mycol.count_documents({})
+    get = mycol.count_documents({"method": "GET"})
+    post  = mycol.count_documents({"method": "POST"})
+    put = mycol.count_documents({"method": "PUT"})
+    patch =  mycol.count_documents({"method": "PATCH"})
+    delete = mycol.count_documents({"method": "DELETE"})
+    path = mycol.count_documents(
+        {"method": "GET", "path": "/status"})
+
+
+    print(f"{total_log} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+
+
+if __name__ == "__main__":
+    log_stat()
